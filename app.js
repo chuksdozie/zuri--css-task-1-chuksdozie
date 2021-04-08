2eac47ad929acfc18a6694f412c1d893fb766609
Chuks dozie <chuksdozie48@gmail.com>
2021-04-08 03:04:52 +0100

    really did a lot mainly on media query for mobile

---
+++ app.js
@@ -0,0 +1,10 @@
+function mymenu(){
+    var menu = document.getElementById("navigationlinks")
+    if (menu.className == "nav-links"){
+        menu.className = "nav-links1"
+    }
+    else{
+        menu.className = "nav-links"
+    }
+}
+
